2c40050bca0f1ee0f3c7c545b6d8f0ced48c3400
XiaoGe <1987105819@qq.com>
2016-11-23 17:56:46 +0800

    是否

--- seconde.php
+++ seconde.php
@@ -4,6 +4,11 @@ $a=1;
 if($a==1){
 	echo "Marry said"."hello word!! 测试流程";
 	echo "Marry said"."hello word!! 测试流程";
+	echo "Marry said"."hello word!! 测试流程";
+	echo "Marry said"."hello word!! 测试流程";
+	echo "Marry said"."hello word!! 测试流程";
+	echo "Marry said"."hello word!! 测试流程";
+	echo "Marry said"."hello word!! 测试流程";
 	echo "Ghost add content";
 	echo "Ghost add content";
 	echo "Ghost add content";
